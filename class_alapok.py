60eb02b285a9f3835907cfccc5bc2cd799c8ae13
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-11-15 13:39:44 +0100

    Create class_alapok.py

---
+++ class_alapok.py
@@ -0,0 +1,19 @@
+class Diak:
+    def __init__(self,kereszt_nev:str,vezetek_nev:str,osztaly:str,nem:str,hianyzas:bool):   # alapértelmezett értékeket is tudunk megadni
+        self.kereszt_nev 	= kereszt_nev
+        self.vezetek_nev 	= vezetek_nev
+        self.osztaly 		= osztaly
+        self.nem 			= nem
+        self.hianyzas       = hianyzas
+        
+        
+jozsi = Diak('Lóczy','Józsi','13.i','férfi',True)
+
+jakabné = Diak('Gipsz','Jakabné','11.i','nő',False)
+
+pistike = Diak('Kovács','Pistike','10.i','férfi',True)
+
+
+
+
+        
